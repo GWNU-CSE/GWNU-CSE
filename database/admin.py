4772cf02546e7f3b1617ad4b24de2a5eda7cad1d
unknown <minbak26@gmail.com>
2024-10-13 21:22:58 +0900

    url 다중저장

--- database/admin.py
+++ database/admin.py
@@ -1,10 +1,14 @@
-
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserURL
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('username', 'nickname', 'url')  # 표시할 필드
-    search_fields = ('username', 'nickname')  # 검색할 수 있는 필드
+    list_display = ('username', 'nickname')
+    search_fields = ('username', 'nickname')
 
+class UserURLAdmin(admin.ModelAdmin):
+    list_display = ('user_profile', 'url')
+    search_fields = ('url',)
 
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserURL, UserURLAdmin)
+
