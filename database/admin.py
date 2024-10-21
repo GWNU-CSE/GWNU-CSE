cfb9d29e5f11bf221498ca393cba51159eed0564
unknown <minbak26@gmail.com>
2024-10-21 14:15:18 +0900

    DB 전체수정

--- database/admin.py
+++ database/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, UserURL
+from .models import UserProfile
 
+@admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('username', 'nickname')
-    search_fields = ('username', 'nickname')
-
-class UserURLAdmin(admin.ModelAdmin):
-    list_display = ('user_profile', 'url')
-    search_fields = ('url',)
-
-admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(UserURL, UserURLAdmin)
-
+    list_display = ('email_id', 'nickname')  # 관리자 페이지에서 표시할 필드
+    search_fields = ('enail_id', 'nickname')  # 검색 필드 추가
--- database/models.py
+++ database/models.py
@@ -1,34 +1,19 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import make_password
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 
 class UserProfile(models.Model):
-    username = models.EmailField(unique=True)  # 이메일 형태의 아이디
+    email_id = models.EmailField(unique=True)  # 이메일 형태의 아이디
     password = models.CharField(max_length=128)  # 비밀번호 해시 저장
     nickname = models.CharField(max_length=50)  # 닉네임
+    urls = models.JSONField(default=list, blank=True)  # URL 리스트 저장
+
+    def __str__(self):
+        return self.email_id
 
     def save(self, *args, **kwargs):
         # 비밀번호 해시 처리
-        if not self.pk or 'password' in kwargs:  # 새 객체이거나 비밀번호가 변경될 때
+        if not self.pk or not self.password.startswith('pbkdf2_'):  # 새 객체이거나 비밀번호가 해시되지 않은 경우
             self.password = make_password(self.password)
         super().save(*args, **kwargs)
-
-    def check_password(self, raw_password):
-        """ 주어진 비밀번호가 저장된 해시 비밀번호와 일치하는지 확인 """
-        return check_password(raw_password, self.password)
-
-class UserURL(models.Model): #3. url 별도저장
-    user_profile = models.ForeignKey(UserProfile, related_name='urls', on_delete=models.CASCADE)
-    url = models.CharField(max_length=2048)
-
-    def clean(self):
-        """ URL 유효성 검사 """
-        validator = URLValidator()
-        try: 
-            validator(self.url)
-        except ValidationError:
-            raise ValidationError(f"유효하지 않은 URL입니다: {self.url}")
-
-    def __str__(self):
-        return self.url
